265be7a94d8af9143dd0203051d008fab64c09a0
Krzysztof Smyk <k.smyk@samsung.com>
2019-08-22 16:36:21 +0200

    Update on project

--- app/src/main/java/com/example/not2/MainActivity.java
+++ app/src/main/java/com/example/not2/MainActivity.java
@@ -1,19 +1,28 @@
 package com.example.not2;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.NotificationCompat;
-
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
-import android.os.Build;
 import android.os.Bundle;
-import android.service.notification.NotificationListenerService;
-import android.util.Log;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
 public class MainActivity extends AppCompatActivity {
-    public static final String CHANNEL_1_ID = "channel1";
+    // ==================================
+    // MUST BE THE SAME ON BOTH APPS
+    // ==============================================================
+    private static final int NOTIF_ID = 1;
+    public static final String NOTIF_TAG = "TAG_YETI";
+    public static final String NOTIF_CHANNEL_ID = "notif_channel_id";
+    // ===============================================================
+
+    // =====================================================================
+    // FIXME IMPORTANT: App team must give to as theirs APP ID from GRADLE
+    // =====================================================================
+    public static final String SECOND_APP_ID = "com.example.not1";
+    private static final String NOTIF_CHANNEL_NAME = "notif_channel_name_2";
+    private static final String NOTIF_CHANNEL_DESCRIPTION = "This is channel 2";
     private NotificationManager notificationManager;
 
     @Override
@@ -21,49 +30,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
         createNotificationChanels();
 
-
-        notificationManager= (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-        notificationManager.cancel(1);
-
-
-        Notification notification = new Notification.Builder(this, MainActivity.CHANNEL_1_ID)
+        Notification notification = new Notification.Builder(this, MainActivity.NOTIF_CHANNEL_ID)
                 .setSmallIcon(R.drawable.ic_announcement_black_24dp)
                 .setContentTitle("cccc")
                 .setContentText("dddd")
                 .setCategory(NotificationCompat.CATEGORY_MESSAGE)
-                .setAutoCancel(true)
+                .setAutoCancel(false)
                 .build();
 
+        // TODO: Below line means nothing
+        // it works without Delegate
+        //notificationManager.setNotificationDelegate(SECOND_APP_ID);
 
-
-        Log.i("not2",String.valueOf(notificationManager.canNotifyAsPackage("com.example.not1")));
-        notificationManager.setNotificationDelegate("com.example.not1");
-        notificationManager.notifyAsPackage("com.example.not1","dupa",1,notification);
-
-
-
-
-        //Toast.makeText(getApplicationContext(),"SECOND SERVICE",Toast.LENGTH_SHORT).show();
+        notificationManager.notifyAsPackage(SECOND_APP_ID,
+                NOTIF_TAG,
+                NOTIF_ID,
+                notification);
     }
 
+    private void createNotificationChanels() {
+        NotificationChannel channel = new NotificationChannel(
+                NOTIF_CHANNEL_ID,
+                NOTIF_CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_HIGH
+        );
+        channel.setDescription(NOTIF_CHANNEL_DESCRIPTION);
 
-
-
-
-
-    private void createNotificationChanels(){
-            NotificationChannel channel1 = new NotificationChannel(
-                    CHANNEL_1_ID,
-                    "Channel 1",
-                    NotificationManager.IMPORTANCE_HIGH
-            );
-            channel1.setDescription("This is channel 1");
-
-            NotificationManager manager = getSystemService(NotificationManager.class);
-            manager.createNotificationChannel(channel1);
-
-
+        notificationManager.createNotificationChannel(channel);
     }
 }
